1e46e487b5f7f557e1106f25a858eaef3f86a460
angelcepeda <jangelcepeda@protonmail.com>
2023-11-30 13:50:25 -0600

    localhost config

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const {Kafka} = require('kafkajs');
 
 const kafka_conection = new Kafka({
     clientId:'my-app',
-    brokers:['18.222.175.149:9092']
+    brokers:['localhost:9092']
 })
 
 module.exports = kafka_conection
